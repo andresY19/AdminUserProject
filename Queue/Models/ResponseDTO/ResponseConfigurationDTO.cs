ed2b05eedac0f2b76fd5525d8e63bf8b6588c3ef
yeffersoncardona <yeffercardona@hotmail.com>
2021-08-15 11:28:58 -0500

    Change DTO

--- Queue/Models/ResponseDTO/ResponseConfigurationDTO.cs
+++ Queue/Models/ResponseDTO/ResponseConfigurationDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Syncfusion.EJ2.Schedule;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,8 @@ namespace Queue.Models.ResponseDTO
         public string token { get; set; }
         public bool IsLogged { get; set; }
         public Nullable<System.Guid> Id_Empresa { get; set; }
+        public List<Agent_GroupHorary> GroupHoraryId  { get; set; }
+        public List<Agent_GroupHoraryDetail> GroupHoraryDetail { get; set; }
 
     }
 }
